743bc91b8df557948cea627c8a73ebbf4dad82f2
Brendan Chermack <101033479+BrendanChermack@users.noreply.github.com>
2023-05-23 22:28:51 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -14,14 +14,14 @@ $(document).ready(function() {
     $("#lHidden").animate({
       opacity: 1,
       left: "0"
-    }, 800); // Adjust the duration (in milliseconds) as needed
+    }, 800); 
+    $("#rHidden")
+        .animate({
+            opacity: 1,
+            right: "0"
+        }, 800)
+        .animate({
+            transition: ".4s"
+        });
+        
   });
-$(document).ready(function() {
-    $("#rHidden").animate({
-      opacity: 1,
-      right: "0"
-    }, 800).animate({
-      transition: "0.4s" 
-    }) // Adjust the duration (in milliseconds) as needed
-  });
-  
